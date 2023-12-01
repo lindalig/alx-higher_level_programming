135d41f0c52a107644068323464de128b4fa524f
linda liguru <lindaliguru@gmail.com>
2023-12-01 10:41:51 -0800

    learning 0x11-python-network_1#

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
+        print("Your email is: {}".format(email))
         print(body)
