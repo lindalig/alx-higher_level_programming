95873d454368f125f021cb7ac3a1faf4ae41f56d
linda liguru <lindaliguru@gmail.com>
2023-12-01 04:23:51 -0800

    learning 0x11-python-network_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+
+print("Body response:")
+print("    - type:", type(content))
+print("    - content:", content)
+print("    - utf8 content:", content.decode('utf-8'))
