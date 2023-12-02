4f3b0f25ec397d58c957309b2454206bb3fe2ddc
linda liguru <lindaliguru@gmail.com>
2023-12-01 22:04:18 -0800

    corrections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+/bin/bash
+
+# Check if URL is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send GET request and display debugging info
+response=$(curl -s -w "\n%{http_code}" "$1")
+statusCode=$(echo "$response" | tail -n1)
+body=$(echo "$response" | sed '$d')
+
+echo "HTTP status code: $statusCode"
+echo "Response body:"
